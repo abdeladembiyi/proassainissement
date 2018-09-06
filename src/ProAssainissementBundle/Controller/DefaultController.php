e008c3d191bce5c44884c506cf4a0d7d24ad4fda
mohammedjellab <mohammedjellab@gmail.com>
2018-09-06 23:01:08 +0100

    First commit

---
+++ src/ProAssainissementBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ProAssainissementBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('ProAssainissementBundle:Default:index.html.twig');
+    }
+}
---
+++ src/ProAssainissementBundle/ProAssainissementBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ProAssainissementBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ProAssainissementBundle extends Bundle
+{
+}
